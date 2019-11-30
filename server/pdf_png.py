3c81768a9ed61f57a5b5c586b735bbb219e4eb10
Aaron <Aaron@dhcp-128-189-215-155.ubcsecure.wireless.ubc.ca>
2019-11-30 12:24:24 -0800

    pngtopdf with bug

--- server/pdf_png.py
+++ server/pdf_png.py
@@ -1,1 +1,21 @@
-"how to convert from pdf to png"
+from pdf2image import convert_from_path, convert_from_bytes
+from PIL import Image
+import tempfile
+import os
+
+def main():
+    ImagePath = '/Users/A/Desktop/Code/LHDTest/Images/'
+
+    if not os.path.exists(ImagePath):
+        os.makedirs(ImagePath)
+    
+    with tempfile.TemporaryDirectory() as path:
+        images_from_path = convert_from_path('/Users/A/Desktop/Code/LHDTest/lec29.pdf', output_folder=path)
+        count = 0
+        for image in images_from_path:
+            print(len(image))
+            image.save("{}/{}.png".format(ImagePath,count), "PNG")
+    
+    
+if __name__ == '__main__':
+    main()
