cf0157f901aca0409b0105e50468af231747f8cb
Cody <codysayo@gmail.com>
2019-11-30 14:01:30 -0800

    Filepond upload complete

--- pdfcolorizer/src/App.js
+++ pdfcolorizer/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FilePond ,registerPlugin } from 'react-filepond';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+import 'filepond/dist/filepond.min.css';
+import { Document, Page } from 'react-pdf'
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+
+
+registerPlugin(FilePondPluginFileValidateType,FilePondPluginImagePreview);
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>PDFColorizer</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Upload your file below
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
-    </div>
+        <FilePond acceptedFileTypes = {['application/pdf', 'image/png', 'image/jpg', 'image/jpeg']}>  
+        </FilePond>
+  </div>
   );
 }
 
