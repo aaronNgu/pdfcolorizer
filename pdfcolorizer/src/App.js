e68f5a334a01f929ff1e7eb09551e6a206b36ac5
Thai Ngoc Nguyen <thaingoc@alumni.ubc.ca>
2019-11-30 16:04:26 -0800

    Add spinner

--- pdfcolorizer/src/App.js
+++ pdfcolorizer/src/App.js
@@ -8,6 +8,8 @@ import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css
 import 'filepond/dist/filepond.min.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
+import { ClipLoader } from 'react-spinners';
+
 import Button from '@material-ui/core/Button';
 
 registerPlugin(FilePondPluginFileValidateType, FilePondPluginImagePreview);
@@ -18,16 +20,9 @@ class App extends React.Component {
     super();
 
     this.state = {
-      files: null
+      files: null,
+      processing: false
     }
-  }
-
-  onChangeHandler = event => {
-    console.log(event.target.files[0])
-    this.setState({
-      selectedFile: event.target.files[0],
-      loaded: 0,
-    })
   }
 
   onUpdateFiles = fileItems => {
@@ -39,12 +34,18 @@ class App extends React.Component {
 
   onClickHandler = () => {
     const data = new FormData()
+    this.setState({
+      processing: true
+    })
     data.append('file', this.state.files[0])
     axios.post("http://localhost:5000/upload", data, {
       // receive two    parameter endpoint url ,form data
     })
       .then(res => { // then print response status
         console.log(res.statusText)
+      })
+      .catch(error => {
+        console.log(error)
       })
   }
 
@@ -64,7 +65,16 @@ class App extends React.Component {
           acceptedFileTypes={['application/pdf', 'image/png', 'image/jpg', 'image/jpeg']}
           onupdatefiles={this.onUpdateFiles}
         />
-        <Button color='primary' variant="contained" onClickHandler={this.onClickHandler}>Process</Button> 
+        {(!this.state.processing) ?
+          <Button color='primary' variant="contained" onClick={this.onClickHandler}>Process</Button> 
+          :
+          <ClipLoader
+          sizeUnit={"px"}
+          size={150}
+          color={'#123abc'}
+          loading={this.state.processing}
+        />
+          }
   
       </div>
     )
